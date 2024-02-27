a771c94ff92d369f1447c0469607e26310506664
hanhttb1 <thienbinhtieu03@gmail.com>
2024-02-27 16:37:40 +0700

    tác giả

--- QLTV/obj/Debug/net6.0-windows/QLTV.AssemblyInfo.cs
+++ QLTV/obj/Debug/net6.0-windows/QLTV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLTV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8df278d22dad8b48474d1b0f3994128f61583f5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6976d1ef7680316c70ace5af9f5d2ef3efa91b70")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLTV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLTV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
